bc5b20135ef9cb3c25e97724082ad3590392f407
Ben White <b.white@haileybury.com>
2024-12-09 22:25:06 +0000

    Day 09 updates

--- day_template.py
+++ day_template.py
@@ -1,5 +1,11 @@
 import time
 start_time = time.time()
+def print_elapsed_time():
+    elapsed_time = time.time() - start_time
+    if elapsed_time < 0.1:
+        print("%s ms" % float("%.3g" % (elapsed_time * 1000)))
+    else:
+        print("%s s" % float("%.3g" % elapsed_time))
 
 file = open("inputs/input_test.txt", "r")
 
